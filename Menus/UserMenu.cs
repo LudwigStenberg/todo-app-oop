8154fea3ce10ef5994a96d78a6794dffad7b2632
Andreas <andreas_fagerberg@outlook.com>
2024-10-15 14:45:56 +0200

    Added HelpCommand to usermenu

--- Menus/UserMenu.cs
+++ Menus/UserMenu.cs
@@ -1,6 +1,8 @@
 public class UserMenu : Menu
 {
-    public UserMenu(Program program) : base(program) {
+    public UserMenu(Program program)
+        : base(program)
+    {
         // Eftersom 'AddCommand' kommer från 'Menu' (bas-klassen) så kan man
         // använda 'base' för att referera till den
         base.AddCommand(new CreateTaskCommand(program));
@@ -8,7 +10,7 @@ public class UserMenu : Menu
         // Men samtidigt, eftersom det endast finns en 'AddCommand' så
         // behövs det inte:
         // AddCommand(new CreateTaskCommand());
-
+        base.AddCommand(new HelpCommand(program));
         base.AddCommand(new RemoveTaskCommand(program));
         base.AddCommand(new SearchTaskCommand(program));
     }
